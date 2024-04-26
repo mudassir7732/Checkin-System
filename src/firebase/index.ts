3a8df769e8de82d62f8bad822634bc57abccc190
mudassir7732 <mudassir7732@gmail.com>
2024-04-26 13:28:54 +0500

    Replaced firebase configuration credentials

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAHEQdNHtgz4Dj7mNM3xTozNqedSGUEIHU",
-  authDomain: "wonder-crafts-2c771.firebaseapp.com",
-  projectId: "wonder-crafts-2c771",
-  storageBucket: "wonder-crafts-2c771.appspot.com",
-  messagingSenderId: "811035487193",
-  appId: "1:811035487193:web:de28417d48231a80fe47c8"
+  apiKey: "***************************************",
+  authDomain: "***********************************",
+  projectId: "*******************",
+  storageBucket: "*******************************",
+  messagingSenderId: "************",
+  appId: "*****************************************"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
