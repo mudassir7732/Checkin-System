6841870bac12c5517c5fd7c4a728038ef04d2390
mudassir7732 <mudassir7732@gmail.com>
2024-06-18 01:56:06 +0500

    Updated env file

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+require('dotenv').config();
 
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
@@ -13,3 +14,24 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
 export default db;
+
+
+
+
+
+
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAHEQdNHtgz4Dj7mNM3xTozNqedSGUEIHU",
+//   authDomain: "wonder-crafts-2c771.firebaseapp.com",
+//   projectId: "wonder-crafts-2c771",
+//   storageBucket: "wonder-crafts-2c771.appspot.com",
+//   messagingSenderId: "811035487193",
+//   appId: "1:811035487193:web:de28417d48231a80fe47c8"
+// };
+
+// const firebaseApp = initializeApp(firebaseConfig);
+// const db = getFirestore(firebaseApp);
+// export default db;
