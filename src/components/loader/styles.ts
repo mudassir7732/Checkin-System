483b3b74c3c725990d1b7d823216d0c75e5f633b
mudassir7732 <mudassir7732@gmail.com>
2024-06-18 17:05:14 +0500

    Updated code

--- src/components/loader/styles.ts
+++ src/components/loader/styles.ts
@@ -1,5 +1,5 @@
 const styles={
-    container:"loader tw-fixed tw-top-0 tw-left-0 tw-w-full tw-h-full tw-z-50 tw-flex tw-justify-center tw-items-center tw-bg-black tw-bg-opacity-50",
+    container:"tw-pointer-events-none tw-fixed tw-top-0 tw-left-0 tw-w-full tw-h-full tw-z-50 tw-flex tw-justify-center tw-items-center tw-bg-black tw-bg-opacity-50",
     firstBlock:"tw-h-24 tw-w-24 tw-rounded-full tw-border-t-8 tw-border-b-8 tw-border-gray-200",
     secondBlock:"tw-absolute tw-top-0 tw-left-0 tw-h-24 tw-w-24 tw-rounded-full tw-border-t-8 tw-border-b-8 tw-border-blue-500 animate-spin",
 }
